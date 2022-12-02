12d8b64c0c5826968e362267de74b18dad61bbb8
MarBor2000 <73531965+MarBor2000@users.noreply.github.com>
2022-12-02 22:26:07 +0100

    Add project files.

---
+++ Build Your Form/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Build_Your_Form.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
